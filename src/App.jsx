543cd7807894772fd71ace3539e3c5ab37d0f26a
zainab <zainabhammaad@gmail.com>
2025-02-19 20:37:22 +0300

    View book lists

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,38 @@ import SignUpForm from './components/SignUpForm/SignUpForm'
 import Landing from './components/Landing/Landing'
 import SignInForm from './components/SignInForm/SignInForm'
 import Dashboard from './components/Dashboard/Dashboard'
+import BookList from './components/BookList/BookList';
+
+import * as bookService from './services/bookService';
+import { useState, useEffect } from 'react';
 
 import { UserContext } from './contexts/UserContext'
 
-const App = () => {
 
+
+const App = () => {
+  
   const { user } = useContext(UserContext)
+  const [books, setBooks] = useState([]);
+
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      const booksData = await bookService.index();
+
+      setBooks(booksData);
+    };
+    if (user) fetchAllBooks();
+  }, [user]);
+
 
   return (
     <>
       <NavBar />
       <Routes>
         {/* if user exists show dashboard otherwise show landing page */}
+        <Route path='/books' element={<BookList books={books} />} />
+
         <Route path='/' element={user ? <Dashboard /> : <Landing />} />
         <Route path='/sign-in' element={<SignInForm />} />
         <Route path='/sign-up' element={<SignUpForm />} />
